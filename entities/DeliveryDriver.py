13f4ce865cd50a57963c8b43e775e052345fe917
S010MON <leon@bdebnath.com>
2021-09-20 21:27:21 +0200

    Delivery Driver

---
+++ entities/DeliveryDriver.py
@@ -0,0 +1,7 @@
+
+class DeliveryDriver:
+
+    def __init__(self, name, operating_area, on_task):
+        self.name = name
+        self.operating_area = operating_area
+        self.on_task = on_task

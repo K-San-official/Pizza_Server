e1fec3aa9e62c892033381dd5e2aed2297bbb47c
S010MON <leon@bdebnath.com>
2021-09-20 18:19:47 +0200

    Logic -> Controller

--- PizzaService.py
+++ PizzaService.py
@@ -1,5 +1,5 @@
 import json
-import Logic
+import PizzaController
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 
@@ -8,22 +8,22 @@ api = Api(app)
 
 class Pizza(Resource):
     def get(self):
-        return Logic.get_all_pizzas()
+        return PizzaController.get_all_pizzas()
 
 class Drink(Resource):
     def get(self):
-        return Logic.get_all_drinks()
+        return  PizzaController.get_all_drinks()
 
 class Desert(Resource):
     def get(self):
-        return Logic.get_all_deserts()
+        return PizzaController.get_all_deserts()
 
 class Customer(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('customer_id', type=int)
         args = parser.parse_args()
-        return Logic.get_customer_by_id(args['customer_id']),200
+        return PizzaController.get_customer_by_id(args['customer_id']),200
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -33,11 +33,11 @@ class Customer(Resource):
         parser.add_argument('town', type=str)
         parser.add_argument('postcode', type=str)
         args = parser.parse_args()
-        return Logic.get_customer_by_id(args['customer_id']),200
+        return PizzaController.get_customer_by_id(args['customer_id']),200
 
 class Order(Resource):
     def get(self):
-        return Logic.get_order_by_id(1), 200
+        return PizzaController.get_order_by_id(1), 200
 
     def post(self):
         # Add making a new order

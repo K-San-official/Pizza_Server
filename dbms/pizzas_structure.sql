60f1fa4b23516a3257c1db87d978eb1aacdb07e7
k-san-official <konstantin.sandfort@gmail.com>
2021-09-20 19:37:18 +0200

    add create database SQL

--- dbms/pizzas_structure.sql
+++ dbms/pizzas_structure.sql
@@ -3,10 +3,83 @@ CREATE DATABASE pizzas;
 USE pizzas;
 
 -- Create pizzza table
-create table Pizza (
-    pizza_id int NOT NULL AUTO_INCREMENT,
-    pizza_name varchar(255) NOT NULL,
-    pizza_price_euros int NOT NULL,
-    pizza_price_cents int NOT NULL, PRIMARY KEY (pizza_id));
+CREATE TABLE Pizza (
+    pizza_id INT NOT NULL AUTO_INCREMENT,
+    pizza_name VARCHAR(255) NOT NULL,
+    pizza_price_euros INT NOT NULL,
+    pizza_price_cents INT NOT NULL,
+    PRIMARY KEY (pizza_id));
+
+CREATE TABLE Drink (
+    drink_id INT NOT NULL AUTO_INCREMENT,
+    drink_name VARCHAR(255) NOT NULL,
+    drink_price_euros INT NOT NULL,
+    drink_price_cents INT NOT NULL,
+    PRIMARY KEY (drink_id));
+
+CREATE TABLE Dessert (
+    dessert_id INT NOT NULL AUTO_INCREMENT,
+    dessert_name VARCHAR(255) NOT NULL,
+    dessert_price_euros INT NOT NULL,
+    dessert_price_cents INT NOT NULL,
+    PRIMARY KEY (dessert_id));
+
+CREATE TABLE ToppingMapping (
+    topping_map_id INT NOT NULL,
+    pizza_id INT NOT NULL,
+    topping_id INT NOT NULL,
+    PRIMARY KEY (topping_map_id));
+
+CREATE TABLE PizzaMapping (
+    pizza_map_id INT NOT NULL,
+    order_id INT NOT NULL,
+    pizza_id INT NOT NULL,
+    PRIMARY KEY (pizza_map_id));
+
+CREATE TABLE DrinkMapping (
+    drink_map_id INT NOT NULL,
+    order_id INT NOT NULL,
+    drink_id INT NOT NULL,
+    PRIMARY KEY (drink_map_id));
+
+CREATE TABLE DessertMapping (
+    dessert_map_id INT NOT NULL,
+    order_id INT NOT NULL,
+    dessert_id INT NOT NULL,
+    PRIMARY KEY (dessert_map_id));
+
+CREATE TABLE Topping (
+    topping_id INT NOT NULL,
+    topping_name VARCHAR(255) NOT NULL,
+    vegetarian BOOL NOT NULL,
+    PRIMARY KEY (topping_id));
+
+CREATE TABLE Order (
+    order_id INT NOT NULL,
+    ordered_at DATETIME NOT NULL,
+    customer_id INT NOT NULL,
+    delivery_driver_id INT,
+    primary key (order_id));
+
+CREATE TABLE Customer (
+    customer_id INT NOT NULL,
+    name VARCHAR(255),
+    address_id INT NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    primary key (customer_id));
+
+CREATE TABLE DeliveryDriver (
+    driver_id INT NOT NULL,
+    operating_area VARCHAR(6) NOT NULL,
+    on_task BOOL NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    primary key (driver_id));
+
+CREATE TABLE Address (
+    address_id INT NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    town VARCHAR(255) NOT NULL,
+    postcode VARCHAR(6) NOT NULL,
+    primary key (address_id));
 
 

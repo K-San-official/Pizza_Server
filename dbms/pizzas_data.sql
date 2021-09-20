55fc1c058a325b918c91ef74935be88fce6e5562
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2021-09-20 18:07:53 +0200

    Create pizzas_data.sql

---
+++ dbms/pizzas_data.sql
@@ -0,0 +1,11 @@
+-- Insert pizzas into pizza table
+INSERT INTO pizza (name, price) VALUES ('Margherita', 8.50);
+INSERT INTO pizza (name, price) VALUES ('Marinara', 9.50);
+INSERT INTO pizza (name, price) VALUES ('Napoletana', 9.50);
+INSERT INTO pizza (name, price) VALUES ('Quattro Formaggi', 11.00);
+INSERT INTO pizza (name, price) VALUES ('Frutti di Mare', 12.00);
+INSERT INTO pizza (name, price) VALUES ('Prosciutto', 10.50);
+INSERT INTO pizza (name, price) VALUES ('Mediterranea', 10.50);
+INSERT INTO pizza (name, price) VALUES ('Vegetariana', 8.50);
+INSERT INTO pizza (name, price) VALUES ('Rustica', 9.50);
+INSERT INTO pizza (name, price) VALUES ('Pizza al Pesto', 9.90);

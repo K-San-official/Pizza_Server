491b4a66fdcc510ad14e36657bcf3b3254b475fe
S010MON <leon@bdebnath.com>
2021-09-20 21:22:25 +0200

    Customer and address added

---
+++ entities/Address.py
@@ -0,0 +1,8 @@
+
+class Address:
+
+    def __init__(self, house_no, street, town, postcode):
+        self.house_no = house_no
+        self.stree = street
+        self.town = town
+        self.postcode = postcode

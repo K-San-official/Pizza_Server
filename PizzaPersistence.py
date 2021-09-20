6b463bcad00bfe630393fb42e1336e2a09a7117a
k-san-official <konstantin.sandfort@gmail.com>
2021-09-20 21:32:32 +0200

    blank methods for PizzaPersistence.py

--- PizzaPersistence.py
+++ PizzaPersistence.py
@@ -1,15 +1,42 @@
+import entities.Pizza
 import mysql
 from mysql.connector import (connection)
 
 cnx = mysql.connector.connect(user='root', password='password',
                               host='127.0.0.1',
                               database='PizzaDB')
-cnx.close()
+cursor = cnx.cursor()
 
 # required methods
 
 def get_all_pizzas():
-    query = ("SELECT pizza_name FROM Pizza ")
+    query = ("SELECT pizza_name, pizza_price_euros, pizza_price_cents FROM Pizza")
+    cursor.execute(query)
+    for pizzaEntry in cursor:
+        entities.Pizza(pizzaEntry.name, pizzaEntry.pizza_price_euro, pizzaEntry.pizza_price_cents, pizzaEntry.toppings)
+    return None
+
+def get_all_desserts():
+    return None
+
+def get_all_drinks():
+    return None
+
+def create_purchase(Purchase):
+    return None
+
+def create_customer(Customer):
+    return None
+
+def get_customer_address(Customer):
+    return None
+
+def get_purchase(purchase_id):
+    return None
+
+def get_delivery_driver(purchase_id):
+    return None
+
 """
 - get_all_pizzas() -> Pizza[]
 - get_all_desserts() -> Desert[]

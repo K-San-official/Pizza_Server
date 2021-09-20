2c1c0a153f936d356f301f80b19a2684f6cdc50a
k-san-official <konstantin.sandfort@gmail.com>
2021-09-20 18:15:34 +0200

    rename logic to controller

--- PizzaService.py
+++ PizzaService.py
@@ -1,31 +1,29 @@
 import json
-import Logic
+import PizzaController
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-
-
 class Pizza(Resource):
     def get(self):
-        return Logic.get_all_pizzas()
+        return PizzaController.get_all_pizzas()
 
 class Drink(Resource):
     def get(self):
-        return Logic.get_all_drinks()
+        return PizzaController.get_all_drinks()
 
 class Desert(Resource):
     def get(self):
-        return Logic.get_all_deserts()
+        return PizzaController.get_all_deserts()
 
 class Customer(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('customer_id', type=int)
         args = parser.parse_args()
-        return Logic.get_customer_by_id(args['customer_id']),200
+        return PizzaController.get_customer_by_id(args['customer_id']),200
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -35,11 +33,11 @@ class Customer(Resource):
         parser.add_argument('town', type=str)
         parser.add_argument('postcode', type=str)
         args = parser.parse_args()
-        return Logic.get_customer_by_id(args['customer_id']),200
+        return PizzaController.get_customer_by_id(args['customer_id']),200
 
 class Order(Resource):
     def get(self):
-        return Logic.get_order_by_id(1), 200
+        return PizzaController.get_order_by_id(1), 200
 
     def post(self):
         # Add making a new order
@@ -51,5 +49,4 @@ api.add_resource(Desert, '/desert')
 api.add_resource(Customer, '/customer')
 api.add_resource(Order, '/order')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
+import PizzaService
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
 if __name__ == '__main__':
-    app.run()
+    PizzaService.run(Debug=True)

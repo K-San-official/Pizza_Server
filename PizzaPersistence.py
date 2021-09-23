4b689262a81911b685a235270f7788db33f1fae6
k-san-official <konstantin.sandfort@gmail.com>
2021-09-23 11:25:23 +0200

    added dessert and drink db queries

--- PizzaPersistence.py
+++ PizzaPersistence.py
@@ -1,4 +1,5 @@
 from entities.Pizza import Pizza
+from entities.Desert import Desert
 from entities.Drink import Drink
 import mysql
 from mysql.connector import (connection)
@@ -14,24 +15,33 @@ config = {
 }
 
 cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
 
 # required methods
 
 def get_all_pizzas():
-    cursor = cnx.cursor()
     query = ("SELECT pizza_name, pizza_price_euros, pizza_price_cents FROM Pizza;")  # need to add join for toppings
     cursor.execute(query)
     AllPizzas = []
     for (pizza_name, pizza_price_euros, pizza_price_cents) in cursor:
         AllPizzas.append(Pizza(pizza_name, pizza_price_euros, pizza_price_cents, []))  # add toppings
-    cursor.close()
     return AllPizzas
 
 def get_all_desserts():
-    return None
+    query = ("SELECT dessert_name, dessert_price_euros, dessert_price_cents FROM Dessert;")  # need to add join for toppings
+    cursor.execute(query)
+    AllDesserts = []
+    for (dessert_name, dessert_price_euros, dessert_price_cents) in cursor:
+        AllDesserts.append(Drink(dessert_name, dessert_price_euros, dessert_price_cents))  # add toppings
+    return AllDesserts
 
 def get_all_drinks():
-    return None
+    query = ("SELECT drink_name, drink_price_euros, drink_price_cents FROM Drink;")  # need to add join for toppings
+    cursor.execute(query)
+    AllDrinks = []
+    for (drink_name, drink_price_euros, drink_price_cents) in cursor:
+        AllDrinks.append(Drink(drink_name, drink_price_euros, drink_price_cents))  # add toppings
+    return AllDrinks
 
 def create_purchase(Purchase):
     return None
@@ -48,22 +58,20 @@ def get_purchase(purchase_id):
 def get_delivery_driver(purchase_id):
     return None
 
-def testGet():
-    print("Test")
-    cursor = cnx.cursor()
-    query = ("SELECT pizza_name, pizza_price_euros, pizza_price_cents FROM Pizza;")
-    cursor.execute(query)
-    for (pizza_name, pizza_price_euros, pizza_price_cents) in cursor:
-        print(pizza_name, pizza_price_euros, pizza_price_cents)
-    cursor.close()
-
-
 # Main function to test query methods
 if __name__ == '__main__':
-    x = get_all_pizzas()
-    for pizza in x:
+    for pizza in get_all_pizzas():
         print(pizza.name)
 
+    print("---")
+
+    for drink in get_all_drinks():
+        print(drink.name)
+
+    print("---")
+
+    for dessert in get_all_desserts():
+        print(dessert.name)
 
 
 """
